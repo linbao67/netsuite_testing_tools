31c263eb68e615b0debb89e1d371430621341370
linbao67 <lin.bao@hotmail.com>
2020-02-28 12:33:58 +0800

    fix username, password

--- main/netsuite/read_xml.py
+++ main/netsuite/read_xml.py
@@ -1,16 +1,21 @@
-from main.netsuite.netsuite_utils import get_all, passport
-import main.netsuite.endpoints as ep
 import requests
+
+import main.netsuite.endpoints as ep
+from main.netsuite.netsuite_utils import get_all, passport
+
+# input the email address
+email = ''
+password = 'Admin12345'
+realm = 'TSTDRV2177818'
 
 
 class NetSuite(object):
 
     @staticmethod
     def generate_currency():
-
         endpoint = 'currency'
         soap_service_xml = get_all(ep.ENDPOINTS_FIELDS[endpoint][ep.RECORD_TYPE],
-                                   _soapheaders={"tokenPassport": passport()})
+                                   _soapheaders={"tokenPassport": passport(email, password, realm)})
         service = ep.ENDPOINTS_FIELDS[endpoint][ep.SERVICE]
 
         headers = {"Content-Type": "application/soap+xml; charset=UTF-8",
@@ -25,7 +30,6 @@ class NetSuite(object):
 
         print(response)
 
+
 if __name__ == '__main__':
     NetSuite.generate_currency()
-
-
